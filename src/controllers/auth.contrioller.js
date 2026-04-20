e13ae1578c41ec0d48e4d1a5e15df79da103e83f
Qasim <mianqasim065@gmail.com>
2026-04-20 17:44:25 +0500

    feat(auth): complete user login API endpoint

--- src/controllers/auth.contrioller.js
+++ src/controllers/auth.contrioller.js
@@ -3,39 +3,79 @@ const jwt = require("jsonwebtoken");
 async function userRegisterController(req, res) {
   const { userName, password, email } = req.body;
 
-  if (!userName || !password || !email) {
-    return res.status(400).json({
-      message: "username, email and password are require",
+  try {
+    if (!userName || !password || !email) {
+      return res.status(400).json({
+        message: "username, email and password are require",
+      });
+    }
+    const isUserAlreadyExist = await userModel.findOne({
+      $or: [{ email }, { userName }],
+    });
+    if (isUserAlreadyExist) {
+      return res.status(400).json({
+        message: "User with this email or password already exist",
+      });
+    }
+
+    const user = await userModel.create({
+      userName,
+      email,
+      password,
+    });
+
+    const token = jwt.sign(
+      { id: user._id, userName: user.userName },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" },
+    );
+
+    res.cookie("token", token);
+
+    res.status(201).json({
+      message: "User register successfully",
+      id: user._id,
+      userName: user.userName,
+      email: user.email,
+    });
+  } catch (error) {
+    console.error("Registration Error:", error);
+    res.status(500).json({
+      message: "Internal server error during registration",
     });
   }
-  const isUserAlreadyExist = await userModel.findOne({
-    $or: [{ email }, { userName }],
-  });
-  if (isUserAlreadyExist) {
+}
+
+async function userLoginController(req, res) {
+  const { email, password } = req.body;
+  if (!email || !password) {
     return res.status(400).json({
-      message: "User with this email or password already exist",
+      message: "Email and password are required to login",
     });
   }
 
-  const user = await userModel.create({
-    userName,
-    email,
-    password,
-  });
+  const isUserWithEmailExist = await userModel
+    .findOne({
+      email,
+    })
+    .select("+password");
 
-  const token = jwt.sign(
-    { id: user._id, userName: user.userName },
-    process.env.JWT_SECRET,
-    { expiresIn: "1d" },
-  );
+  if (!isUserWithEmailExist) {
+    return res.status(400).json({
+      message: "User not found",
+    });
+  }
 
-  res.cookie("token", token);
+  const isPasswordValid = await isUserWithEmailExist.comparePassword(password);
 
-  res.status(201).json({
-    message: "User register successfully",
-    userName: user.userName,
-    email: user.email,
+  if (!isPasswordValid) {
+    return res.status(400).json({
+      message: "Invalid email or password",
+    });
+  }
+  res.status(200).json({
+    message: "Login successfull",
   });
 }
 
-module.exports = { userRegisterController };
+module.exports = { userRegisterController, userLoginController };
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -3,5 +3,6 @@ const express = require("express");
 const route = express.Router();
 
 route.post("/register", authController.userRegisterController);
+route.post("/login", authController.userLoginController);
 
 module.exports = route;
