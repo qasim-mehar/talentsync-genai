ccb7515626248f650e8ceaa1282c889e4f98c095
Qasim <mianqasim065@gmail.com>
2026-04-22 19:28:19 +0500

    feat(cors): implemet cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
 const authRoute = require("./routes/auth.route");
+const cors= require("cors")
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+    origin:"http://localhost/5173",
+    withCredentials:true
+}))
 app.use(cookieParser());
+
 
 app.use("/api/auth", authRoute);
 
