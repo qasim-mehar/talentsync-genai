ed9ac513bd8f3fc9d53a54264ddd50060a2a9b33
Qasim <mianqasim065@gmail.com>
2026-04-25 15:35:54 +0500

    feat(routing): secure homepage route with protected wrapper

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { LoginPage } from "./features/auth/pages/LoginPage";
 import { RegisterPage } from "./features/auth/pages/RegisterPage";
+import Protected from "./features/auth/components/Protected";
 
 export function AppRoutes() {
   return (
     <Routes>
       {/* Redirect root to login for now */}
-      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/" element={<Protected>{<h1>Homepage</h1>}</Protected>} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       {/* Fallback route */}
